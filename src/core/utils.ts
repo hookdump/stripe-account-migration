c0b19ade04246e34f1c43d4edd42752d9aa7814c
Ignacio Freiberg <ig@toptal.com>
2023-06-01 06:51:23 -0300

    remove debugging code from calculateRemainingDuration

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,5 +1,4 @@
 export function calculateRemainingDuration(startDate: Date, couponDuration: number) {
-  return 2;
   const currentDate: Date = new Date();
   const start: Date = new Date(startDate);
   const elapsedMonths = Math.round((+currentDate - +start) / (1000 * 60 * 60 * 24 * 30));
